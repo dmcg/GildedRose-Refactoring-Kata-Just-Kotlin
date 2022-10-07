51edc8bbda2716befc9f13edc554ccca462e1ece
Duncan McGregor <duncan@oneeyedmen.com>
2022-10-07 10:57:25 +0100

    Rationalise tests

--- Kotlin/src/test/kotlin/com/gildedrose/TexttestFixture.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/TexttestFixture.kt
@@ -1,10 +1,12 @@
 package com.gildedrose
 
-fun main(args: Array<String>) {
+import org.junit.jupiter.api.Test
 
-    println("OMGHAI!")
-
-    val items = arrayOf(Item("+5 Dexterity Vest", 10, 20), //
+class RegressionTest {
+    @Test
+    fun test() {
+        val items = arrayOf(
+            Item("+5 Dexterity Vest", 10, 20), //
             Item("Aged Brie", 2, 0), //
             Item("Elixir of the Mongoose", 5, 7), //
             Item("Sulfuras, Hand of Ragnaros", 0, 80), //
@@ -13,24 +15,20 @@ fun main(args: Array<String>) {
             Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
             Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
             // this conjured item does not work properly yet
-            Item("Conjured Mana Cake", 3, 6))
+            Item("Conjured Mana Cake", 3, 6)
+        )
 
-    val app = GildedRose(items)
+        val app = GildedRose(items)
 
-    var days = 2
-    if (args.size > 0) {
-        days = Integer.parseInt(args[0]) + 1
-    }
-
-    for (i in 0..days - 1) {
-        println("-------- day $i --------")
-        println("name, sellIn, quality")
-        for (item in items) {
-            println(item)
+        val days = 2
+        for (i in 0..days - 1) {
+            println("-------- day $i --------")
+            println("name, sellIn, quality")
+            for (item in items) {
+                println(item)
+            }
+            println()
+            app.updateQuality()
         }
-        println()
-        app.updateQuality()
     }
-
-
 }
